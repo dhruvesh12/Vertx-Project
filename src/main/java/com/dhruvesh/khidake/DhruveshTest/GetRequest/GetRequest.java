9bb0e7469518cac2f6db9f3a19b163b5542ed95a
dhruveshkhidakeTest <gamingzonedhruv@gmail.com>
2023-07-22 15:15:38 +0530

    Changed Response from User2

--- src/main/java/com/dhruvesh/khidake/DhruveshTest/GetRequest/GetRequest.java
+++ src/main/java/com/dhruvesh/khidake/DhruveshTest/GetRequest/GetRequest.java
@@ -8,7 +8,7 @@ public class GetRequest {
   public static void attach(Router router, PgPool pg, Vertx vertx) {
 
     router.get("/getRequest").handler(ctx->{
-      var result = "Hello World";
+      var result = "It is Not Hello World, It is Hello Earth";
       ctx.response()
         .end(result);
     });
