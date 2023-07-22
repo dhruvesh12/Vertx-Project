2604502ab68f0b9d624419c798231816a632e34f
dhruveshkhidakeTest <140226687+dhruveshkhidakeTest@users.noreply.github.com>
2023-07-22 13:39:28 +0530

    Update MainVerticle.java By User2

--- src/main/java/com/dhruvesh/khidake/DhruveshTest/MainVerticle.java
+++ src/main/java/com/dhruvesh/khidake/DhruveshTest/MainVerticle.java
@@ -22,7 +22,7 @@ public class MainVerticle extends AbstractVerticle {
 
 
     vertx.exceptionHandler(error->{
-      Log.error("Something Went Wrong 1 With Main {}",error.getCause());
+      Log.error("Something Went Wrong 1 with User2 With Main {}",error.getCause());
     });
 
     vertx
